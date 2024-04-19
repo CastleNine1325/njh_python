3a6514e628a7eac46c9ce19427fd2623486dc993
CastleNine1325 <90691934+CastleNine1325@users.noreply.github.com>
2024-04-19 16:35:04 +0900

    Add files via upload
    6주차 수업 추가 내용 있음

--- w6.py
+++ w6.py
@@ -16,6 +16,8 @@ print(f'{year}년은 {yun}입니다.')
 
 
 
+
+
 ### 사용자로부터 연도와 월을 입력받아 그 달의 끝날이 며칠인지 출력하는 프로그램
 
 
@@ -41,6 +43,29 @@ month = int(input('월?'))
 ndays = month_days(year, month)
 
 print(f'{year}년 {month}월은 {ndays}일까지 있습니다.')
+
+
+
+
+#개선된 버전
+
+def is_leap_year(y):
+    if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input('운년 여부를 확인할 연도는?'))
+
+print(f'{year}년은', end='')
+
+if is_leap_year(year):
+    print('윤년입니다.')
+else:
+    print('평년입니다.')
+
+
+
 
 
 
