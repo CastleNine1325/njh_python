a35fdcc28e171a440a2a4c23b65acab5105794ba
CastleNine1325 <90691934+CastleNine1325@users.noreply.github.com>
2024-05-03 17:19:49 +0900

    Add files via upload

--- hw7.py
+++ hw7.py
@@ -8,9 +8,16 @@ while True:
         item_cnt = int(input('수량은? '))
         shopping_bag[item] = item_cnt
         print(f'장바구니에 {item} {item_cnt}개가 담겼습니다.')
+        print()
 
 print(f'>>> 장바구니 보기:{shopping_bag}')
 print()
 print('[검색]')
-ser = input('장바구니에서 확인하고자 하는 상품은? ')
-print(f'{ser}은(는) {shopping_bag.get(ser)}개 담겨 있습니다.')
+search = input('장바구니에서 확인하고자 하는 상품은? ')
+
+if search not in shopping_bag:
+    print(f'{search}은(는) 장바구니에 담겨있지 않습니다.')
+else:
+    print(f'{search}은(는) {shopping_bag.get(search)}개 담겨 있습니다.')
+
+
